753b29dafc18b1952d859e5659af2beea1d90aa1
nohsn <minuee@kormedi.com>
2026-01-14 13:32:23 +0900

    20260114_first

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+      {
+        name: "aiga-image-cache",
+        script: "dist/main.js",
+        watch: false,
+        exec_mode: "cluster",
+        instances: 0,
+        env: {
+          NODE_ENV: "development",
+          LOG_LEVEL: "debug",
+          PORT: 7001
+        },
+        env_production: {
+          NODE_ENV: "production",
+          LOG_LEVEL: "info",
+          PORT: 7001
+        },
+        output: "/home/ubuntu/workspace/logs/aiga-image-cache/out.log",
+        error: "/home/ubuntu/workspace/logs/aiga-image-cache/err.log",
+        log_date_format: "YYYY-MM-DD HH:mm:ss",
+      },
+    ]
+  }
